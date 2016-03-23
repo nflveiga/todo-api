6a09c0f4ac6a63d881ec5e99c683ba92dcae5df9
Nuno Veiga <nflveiga@gmail.com>
2016-03-23 17:16:05 +0000

    versao com post request

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 var express = require("express");
+var bodyParser=require("body-parser");
 var app=express();
 var PORT=process.env.PORT || 3000;
-var todos=[{
-    id:1,
-    description:"Banco HSM",
-    completed:false
-},
-          {
-    id:2,
-    description:"VMER",
-    completed:false   
-          },
-      {
-    id:3,
-    description:"Folga",
-    completed:true   
-          }]
+var todos=[];
+var todoNextId=1;
+
+app.use(bodyParser.json());
+
+
 app.get("/", function(req,res){
     res.send("todo API root");
     
@@ -38,9 +30,19 @@ app.get("/todos/:id",function(req,res){
             break;
         }
     }
-    if (itemFound===false){res.status(404).send("fuck");
+    if (itemFound===false){res.status(404).send("not found");
     }
 })
+
+//app POST todos
+app.post("/todos", function(req,res){
+    var body=req.body;
+    body.id=todoNextId;
+    todoNextId++;
+    todos.push(body)
+    res.json(todos);
+    
+});
 
 app.listen(PORT, function(){
     console.log("express listening to "+PORT);
